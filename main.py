3491c2bea81d5d0b8d70149484793ad886df7e02
ragu <rrrtechie@gmail.com>
2023-10-01 20:20:49 +0530

    changed name

--- main.py
+++ main.py
@@ -76,15 +76,15 @@ def crop_image(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     th, threshed = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY_INV)
 
-    ## (2) Morph-op to remove noise
+    # (2) Morph-op to remove noise
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
     morphed = cv2.morphologyEx(threshed, cv2.MORPH_CLOSE, kernel)
 
-    ## (3) Find the max-area contour
+    # (3) Find the max-area contour
     cnts = cv2.findContours(morphed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
     cnt = sorted(cnts, key=cv2.contourArea)[-1]
 
-    ## (4) Crop and save it
+    # (4) Crop and save it
     x, y, w, h = cv2.boundingRect(cnt)
     dst = image[y:y + h, x:x + w]
     return dst
